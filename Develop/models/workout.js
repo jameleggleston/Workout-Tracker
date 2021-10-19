7fd3c48cf2f746d605eaa4fe54fd066898d588f4
God of War <jamel.eggleston@gmail.com>
2021-10-19 19:55:48 -0400

    fixed workoutSchema

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({    
+const workoutSchema = new Schema({    
     day: {
         type: Date,
         default: Date.now,
