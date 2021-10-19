091c69d2565f604d77ea5f62098e3e9807c4f33a
God of War <jamel.eggleston@gmail.com>
2021-10-19 19:52:47 -0400

    updated workoutRoutes

--- Develop/controllers/api/workoutRoutes.js
+++ Develop/controllers/api/workoutRoutes.js
@@ -16,3 +16,39 @@ router.get('/', async (req, res) => {
 
 });
 
+router.post('/', async ({ body }, res) => {
+    try {
+        const newWorkout = await db.Workout.create(body);
+        res.status(200).json(newWorkout);
+    } catch (err) {
+        err && res.status(500).json(err);
+    }
+});
+
+router.put('/:id', async ({ params, body }, res) => {
+    try {
+        const updatedWorkout = await db.Workout.findByIdAndUpdate(
+            params.id,
+            { $push: { exercises: body } },
+            { new: true }
+        );
+        res.status(200).json(updatedWorkout);
+    } catch (err) {
+        err && res.status(500).json(err);
+    }
+});
+
+router.get('/range', async (req, res) => {
+    try {
+        const workouts = await db.Workout.aggregate([
+            {
+                $addFields: { totalDuration: { $sum: "$exercises.duration" } }
+            }
+        ]);
+        res.status(200).json(workouts);
+    } catch (err) {
+        err && res.status(500).json(err);
+    }
+});
+
+module.exports = router;
