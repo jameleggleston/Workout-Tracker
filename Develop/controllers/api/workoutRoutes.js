8cc00f08d02014feac51a05c7c85d15f57ce99c5
God of War <jamel.eggleston@gmail.com>
2021-10-19 19:35:05 -0400

    added get route to workout route

--- Develop/controllers/api/workoutRoutes.js
+++ Develop/controllers/api/workoutRoutes.js
@@ -1,1 +1,18 @@
-//Start here 
+const router = require('express').Router();
+const db = require('../../models');
+
+
+router.get('/', async (req, res) => {
+    try {
+        const workouts = await db.Workout.aggregate([
+            {
+                $addFields: { totalDuration: { $sum: "$exercises.duration" } }
+            }
+        ]);
+        res.status(200).json(workouts);
+    } catch (err) {
+        err && res.status(500).json(err);
+    }
+
+});
+
